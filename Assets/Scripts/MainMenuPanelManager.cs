5d3aca4f67221fd10b9bc422f82607bc049cf594
Eliezer Rodriguez <97846909+elipierrotnoxiii@users.noreply.github.com>
2024-11-28 22:17:43 -0300

    Logic of the navegation betweend panels

--- Assets/Scripts/MainMenuPanelManager.cs
+++ Assets/Scripts/MainMenuPanelManager.cs
@@ -5,52 +5,42 @@ using UnityEngine;
 public class MainMenuPanelManager : MonoBehaviour
 {
     [Header("Panels")]
-    public GameObject mainMenu; 
+    public GameObject mainMenu;
     public GameObject programacion;
     public GameObject ciencias;
     public GameObject ingles;
 
     private void Start()
     {
-       
-        CloseAllPanels();
-        mainMenu.SetActive(true);
+        ShowPanel("MainMenu"); 
     }
 
-    public void OpenPanel(string panelName)
+    public void ShowPanel(string panelName)
     {
-        Debug.Log("Intentando abrir panel: " + panelName);
-        CloseAllPanels();
+        
+        mainMenu.SetActive(false);
+        programacion.SetActive(false);
+        ciencias.SetActive(false);
+        ingles.SetActive(false);
 
+        
         switch (panelName)
         {
+            case "MainMenu":
+                mainMenu.SetActive(true);
+                break;
             case "Programacion":
                 programacion.SetActive(true);
-                Debug.Log("Panel Programación activado.");
                 break;
             case "Ciencias":
                 ciencias.SetActive(true);
-                Debug.Log("Panel Ciencias activado.");
                 break;
             case "Ingles":
                 ingles.SetActive(true);
-                Debug.Log("Panel Inglés activado.");
                 break;
             default:
                 Debug.LogWarning("El panel solicitado no existe: " + panelName);
                 break;
         }
-        
-        if (mainMenu != null)
-        {
-            mainMenu.SetActive(false);
-        }
-    }
-
-    private void CloseAllPanels()
-    {
-        programacion.SetActive(false);
-        ciencias.SetActive(false);
-        ingles.SetActive(false);
     }
 }
